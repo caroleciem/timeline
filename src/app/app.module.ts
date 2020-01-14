ecaf3cf580161e73efe98d23cbbfb2431b5fa979
Nicolas RAOUL <nicolas.raoul@renault.com>
2020-01-14 11:29:59 +0100

    modifs cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TimelineListComponent } from './timeline-list/timeline-list.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { TimelineCardsComponent } from './timeline-cards/timeline-cards.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,8 +18,9 @@ import { TimelineCardsComponent } from './timeline-cards/timeline-cards.componen
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([   { path: '', component: TimelineListComponent },
-  { path: 'cards/:timelineId', component: TimelineCardsComponent },])
+    RouterModule.forRoot([{path: '', component: TimelineListComponent},
+      {path: 'cards/:timelineId', component: TimelineCardsComponent},]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/timeline-cards/timeline-cards.component.ts
+++ src/app/timeline-cards/timeline-cards.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import{ DatasService } from '../datas.service';
+import { DatasService } from '../datas.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-timeline-cards',
@@ -8,10 +9,19 @@ import{ DatasService } from '../datas.service';
 })
 export class TimelineCardsComponent implements OnInit {
   cardsFounds = this.datasService.cardsFounds;
+  guessDateForm;
 
-  constructor(private datasService: DatasService) { }
+  constructor(
+    private datasService: DatasService,
+    private formBuilder: FormBuilder,
+    ) {
+      this.guessDateForm = this.formBuilder.group({
+        guessedDate: '',
+      });
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(guessDate) {}
 }
